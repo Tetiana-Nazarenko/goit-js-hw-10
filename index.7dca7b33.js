bf04c6f3a5f3cb3793d1abdd647e2685fca7605a
Tetiana-Nazarenko <123865975+Tetiana-Nazarenko@users.noreply.github.com>
2023-07-01 08:59:00 +0000

    Deploying to gh-pages from @ Tetiana-Nazarenko/goit-js-hw-10@4d7f93d9d00964bf21b003b82694a36fddabcb53 🚀

---
+++ index.7dca7b33.js
@@ -0,0 +1,2 @@
+console.log("https://api.thecatapi.com/v1/breeds");document.querySelector(".breed-select"),document.querySelector(".cat-info"),document.querySelector(".loader"),document.querySelector(".error");
+//# sourceMappingURL=index.7dca7b33.js.map
